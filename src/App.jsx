d2f22e94ff7d476f0e13a88518a2cc0527cff819
leslie-liz <lerflizz1314@hotmail.com>
2024-04-19 22:53:48 +0800

    Test API in Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchExercises() {
+      const url = 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList';
+      const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '012d80296emsh0121727cf2a9408p10db9cjsn1daa0451ecce',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      }
+      };
+      const res = await fetch(url, options);
+      const json = await res.json();
+      setData(json)
+    }
+    fetchExercises();
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Header is here</h1>
+      {data.map(d => <p key={d}>{d}</p>)}
     </>
   )
 }
